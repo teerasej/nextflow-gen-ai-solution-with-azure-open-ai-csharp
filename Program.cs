49b9f774827253aafef28e0040bb7ea3ee4cd693
Teerasej <teerasej@gmail.com>
2024-03-24 16:08:39 +0700

    complete commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Note: The Azure OpenAI client library for .NET is in preview.
+// Install the .NET library via NuGet: dotnet add package Azure.AI.OpenAI --version 1.0.0-beta.5
+using Azure;
+using Azure.AI.OpenAI;
+using dotenv.net;
+
+DotEnv.Load();
+
+// 1. Create a new OpenAIClient instance with the endpoint and AzureKeyCredential.
+OpenAIClient client = new OpenAIClient(
+  new Uri("https://openai-nextflow.openai.azure.com/"),
+  new AzureKeyCredential(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")));
+
+
+// 2. Read the user input from the console.
+Console.WriteLine("Enter your message: ");
+string input = Console.ReadLine();
+var newChatMessage = new ChatMessage(ChatRole.User, input);
+
+
+// 3. Create a new ChatMessage instance with the user input.
+Response<ChatCompletions> responseWithoutStream = await client.GetChatCompletionsAsync(
+"gpt-4-32k",
+new ChatCompletionsOptions()
+{
+    Messages =
+  {
+      new ChatMessage(ChatRole.System, @"You are an AI assistant that helps people extract sentiment from the message. Score between 0.0-1.0. Output score and a polite response message that includes the summary of the problem."),
+      new ChatMessage(ChatRole.User, @"วันนี้มีผู้ป่วยเยอะ ทำให้โรงพยาบาลแจกยาช้า และฉันต้องยืน เพราะไม่มีเก้าอี้เหลือให้นั่ง"),      new ChatMessage(ChatRole.Assistant, @"{ ""score"": 0.3, ""response"": ""ขออภัยในความไม่สะดวกที่เกิดขึ้นนะคะ เราจะปรับปรุงการจัดการผู้ป่วยในวันที่คนเยอะ และเพิ่มจำนวนเก้าอี้ให้นั่งให้กับผู้ป่วยค่ะ ขอบคุณที่ให้ข้อมูลเพื่อการปรับปรุงค่ะ"" }"), 
+      newChatMessage
+  },
+    Temperature = (float)0.7,
+    MaxTokens = 800,
+    NucleusSamplingFactor = (float)0.95,
+    FrequencyPenalty = 0,
+    PresencePenalty = 0,
+});
+
+
+// 4. Call the GetChatCompletionsAsync method with the GPT-4 model and ChatCompletionsOptions.
+ChatCompletions response = responseWithoutStream.Value;
+
+// 5. Print the response message from the assistant.
+Console.WriteLine(response.Choices[0].Message.Content);
